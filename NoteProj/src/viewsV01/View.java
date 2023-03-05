8e7cb07eeb587d99aabfec7f86436fb2b667619f
ShadowGreg <shadow-greg@yandex.ru>
2023-03-05 14:49:37 +0300

    Перенёс перключатель команд в отдельный класс

--- NoteProj/src/viewsV01/View.java
+++ NoteProj/src/viewsV01/View.java
@@ -3,12 +3,6 @@ package viewsV01;
 import controllers.Controller;
 import core.models.BaseNote;
 import viewsV01.commandsClass.Performed;
-import viewsV01.commandsClass.vieCommands.deleteNotes.DeleteNotes;
-import viewsV01.commandsClass.vieCommands.help.Help;
-import viewsV01.commandsClass.vieCommands.listNotes.ListNotes;
-import viewsV01.commandsClass.vieCommands.noteCreater.NoteCreator;
-import viewsV01.commandsClass.vieCommands.readNote.ReadNote;
-import viewsV01.commandsClass.vieCommands.updateNote.UpdateNote;
 
 public class View<T1 extends Controller> {
     public static Controller noteController;
@@ -26,30 +20,12 @@ public class View<T1 extends Controller> {
             try {
                 command = StandardOperations.prompt("Введите команду (для спраки введите help): ");
                 System.out.println();
-                Performed commandsFactory = createCommandsByName(command.toUpperCase());
+                Performed commandsFactory = CommandSwitch.createCommandsByName(command.toUpperCase());
                 commandsFactory.execute();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
-    }
-
-    static Performed createCommandsByName(String com) throws Exception {
-        switch (com) {
-            case "HELP":
-                return new Help();
-            case "LIST":
-                return new ListNotes();
-            case "CREATE":
-                return new NoteCreator();
-            case "READ":
-                return new ReadNote();
-            case "UPDATE":
-                return new UpdateNote();
-            case "DELETE":
-                return new DeleteNotes();
-        }
-        throw new Exception("Нет такой команды (наберите HELP).");
     }
 
 
